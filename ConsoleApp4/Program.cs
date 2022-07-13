67d01d42cfc82921fd2d3a11fa0e5f35e907168b
KIM SANG KI <ckacl0233@gmail.com>
2022-07-13 16:58:27 +0900

    C# studies+

--- ConsoleApp4/Program.cs
+++ ConsoleApp4/Program.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using static System.Console;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,35 +8,19 @@ namespace ConsoleApp4
 {
     class Program
     {
-        // 프로그램 실행이 시작되는 곳
-        /*
-        다
-        중
-        행
-        주
-        석
-        */
-        // 단
-        // 일
-        // 행
-        // 주
-        // 석
         static void Main(string[] args)
         {
-            /*
-            if (args.Length == 0)
-            {
-                Console.WriteLine("사용법 : Hello.exe 이름");
-                return;
-            }
-
-            WriteLine("Hello, {0}", args[0]);
-            */
-
             Console.WriteLine("여러분, 안녕하세요?");
             Console.WriteLine("반갑습니다!");
 
+            sbyte a = -10;
+            byte b = 40;
+            Console.WriteLine($"a={a}, b={b}");
 
+            short c = -30000;
+            ushort d = 60000;
+
+            Console.WriteLine($"c={c}, d={d}");
         }
     }
 }
---
+++ ConsoleApp5/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace ConsoleApp5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            sbyte a = -10;
+            byte b = 40;
+
+            Console.WriteLine($"a={a}, b={b}");
+
+            short c = -30000;
+            ushort d = 60000;
+
+            Console.WriteLine($"c={c}, d={d}");
+
+            int e = -1000_0000; // 0이 7개
+            uint f = 3_0000_0000; // 0이 8개
+
+            Console.WriteLine($"e={e}, f={f}");
+
+            long g = -5000_0000_0000; // 0이 11개
+            ulong h = 200_0000_0000_0000_0000; // 0이 18개
+
+            Console.WriteLine($"g={g}, h={h}");
+
+            uint ab = uint.MaxValue;
+
+            Console.WriteLine(ab);
+
+            ab = ab + 1;
+
+            Console.WriteLine(ab);
+            // OverFlow 되는 변수는 0이된다.
+
+        }
+    }
+}
