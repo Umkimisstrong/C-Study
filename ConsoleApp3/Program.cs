b719edd9223e8d2596c097f822523bf349de0471
KIM SANG KI <ckacl0233@gmail.com>
2022-07-13 16:14:34 +0900

    C# Studies

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using static System.Console;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        // 프로그램 실행이 시작되는곳
+        static void Main(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                Console.WriteLine("사용법 : Hello.exe <이름>");
+                return;
+            }
+            WriteLine("Hello, {0}!", args[0]);
+        }
+    }
+}
---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using static System.Console;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        // 프로그램 실행이 시작되는 곳
+        /*
+        다
+        중
+        행
+        주
+        석
+        */
+        // 단
+        // 일
+        // 행
+        // 주
+        // 석
+        static void Main(string[] args)
+        {
+            /*
+            if (args.Length == 0)
+            {
+                Console.WriteLine("사용법 : Hello.exe 이름");
+                return;
+            }
+
+            WriteLine("Hello, {0}", args[0]);
+            */
+
+            Console.WriteLine("여러분, 안녕하세요?");
+            Console.WriteLine("반갑습니다!");
+
+
+        }
+    }
+}
