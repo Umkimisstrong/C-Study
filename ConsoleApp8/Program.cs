259314a51e45300d6588e2673da9654de777331a
KIM SANG KI <ckacl0233@gmail.com>
2022-08-03 11:10:55 +0900

    ?? 연산자

---
+++ ConsoleApp8/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApp8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+
+            // ▶ 연산자 ??
+
+            List<int> num = null;
+            int? a = null;
+
+            (num ??= new List<int>()).Add(5);
+
+            Console.WriteLine(string.Join("▶", num));   // 5
+
+            num.Add(a ??= 0);
+            Console.WriteLine(string.Join("▶", num));   // 5 0
+
+            Console.WriteLine(a);                       // 0
+            
+        }
+    }
+}
